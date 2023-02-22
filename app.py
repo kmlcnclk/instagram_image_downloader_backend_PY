612b340eb14ef21290a44455a72495dc5f50c94f
Kamilcan Çelik <kamilcancelik5757@gmail.com>
2023-02-22 12:19:12 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import requests
+import re
+
+app = Flask(__name__)
+
+
+def get_response(url):
+    r = requests.get(url)
+    while r.status_code != 200:
+        r = requests.get(url)
+    return r.text
+
+
+def prepare_urls(matches):
+    list = []
+    for match in matches:
+        if match.startswith("https:") and "scontent.cdninstagram.com" in match:
+            list.append(match.replace("\\", ""))
+    return list
+
+
+@app.route('/findUrls')
+def find_urls():
+    url = request.args.get("url")
+    response = get_response(url)
+    img_matches = re.findall('"url":"([^"]+)"', response)
+    img_urls = prepare_urls(img_matches)
+    return jsonify(img_urls)
+
+
+if __name__ == '__main__':
+    app.run()
